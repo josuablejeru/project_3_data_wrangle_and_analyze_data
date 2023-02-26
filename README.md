54a6c058dc6f2c71f9275bd2029887f8e26d2f0e
Josua Blejeru <josua@blejeru.com>
2023-02-26 22:59:59 +0100

    :tada: start of the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project 3 Data Wrangle and Analyze Data
+
+goal: wrangle WeRateDogs Twitter data to create interesting and trustworthy analyses and visualizations.
---
+++ extract_twitter_data.py
@@ -0,0 +1,13 @@
+"""
+Gather each tweet's retweet count and favorite ("like") count and saves it to tweet.json
+"""
+__author__ = "Josua Blejeru"
+
+
+def main():
+    """ grab and save all informations about twitter data """
+    ...
+
+
+if __name__ == "__main__":
+    main()
